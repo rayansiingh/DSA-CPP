9570681ebd37921c30a1def4153da5779c665894
Rayan Singh <rayansiingh@gmail.com>
2024-02-03 14:47:47 +0530

    Added basic

---
+++ 3_basic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // basic printing and data types
+    cout << "Hello Coder Army" << endl;
+    cout << "Hello Coder Army\n";
+    cout << "Hello Coder Army\n";
+    cout << 2 + 3;
+
+    int a = 10, b = 20, c = 3; // variabe declare and define
+    cout << a + b * c << "\n";
+
+    b = 1000;
+    cout << b << "\n";
+
+    char x = 'p';
+    cout << x;
+
+    float f = 2.33;
+    cout << f << "\n";
+
+    double d = 2.3332233;
+    cout << d << endl;
+
+    bool t = true; // you can give b 0 or 1 as well
+    cout << t;
+};
