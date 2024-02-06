b54cb4475f1d4831c1c0df20af09099d56eda3df
Rayan Singh <rayansiingh@gmail.com>
2024-02-07 01:21:45 +0530

    Added test file

---
+++ test.cpp
@@ -0,0 +1,22 @@
+// I dump test codes here
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[5];
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> array[i];
+    }
+
+    cout << "values" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cout << array[i] << endl;
+    }
+
+    return 0;
+}
