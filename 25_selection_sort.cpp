bf7d87d6867ecfca7667a2c352ad1252414661dd
Rayan Singh <rayansiingh@gmail.com>
2024-03-30 19:00:14 +0530

    Added selection sort

--- 25_selection_sort.cpp
+++ 25_selection_sort.cpp
@@ -61,6 +61,44 @@ int main()
 //     return 0;
 // }
 
+// now greatest will go to last but loop will start from the beginnign
+// int main()
+// {
+//     // for inserting elements
+//     int arr[1000];
+//     int n;
+//     cout << "enter no of elements: ";
+//     cin >> n;
+//     cout << "enter the elements: ";
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     // main part
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int index = 0;
+//         for (int j = 0; j < n - i; j++)
+//         {
+//             if (arr[j] > arr[index])
+//             {
+//                 index = j;
+//             }
+//         }
+//         swap(arr[index], arr[(n - 1) - i]);
+//     }
+
+//     // for printing ordered array
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
 // for array of characters
 // int main()
 // {
